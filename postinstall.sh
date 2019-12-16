c17929f84f8e2e68797a6a1c724d74a870625d1d
cabi81 <37244289+cabi81@users.noreply.github.com>
2019-12-16 15:17:47 +1100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,8 +2,17 @@
 
 #Info: Simple script that installs additional useful programs to a Debian Netinstall installation.
 #Author: cabi81
-#Version: v0.1
-#Date: 22/07/2018 
+#Version: v0.2
+#Date: 16/12/2019 
+
+##########
+# History
+#
+# v0.1 - Initial Release (22/07/2018)
+# v0.2 - Added Updating sources.list (16/12/2019)
+#		 Added ACYLS Fonts (16/12/2019)
+#
+##########
 
 #Notes: Must be root to run this script!
 
@@ -12,31 +21,43 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
+# Updating sources.list:
+if [ -f /etc/apt/sources.list ]; then
+	sudo sed -i 's|main|main contrib non-free|g' /etc/apt/sources.list
+fi
+
+sleep 10s
+
 # Updating repo:
 apt-get update -y
-	
+
 # Installing WM/X Windows:
 apt-get install i3 openbox obconf obmenu xorg compton -y
-	
+
 # Installing Admin tools:
 apt-get install ssh sudo screen apt-transport-https htop gcc git dstat wireshark linux-headers-amd64 samba -y
-	
+
 # Installing Appearance/Theme tools:
-apt-get install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance gtk-theme-switch gtk-chtheme qt4-qtconfig xfonts-terminus ttf-mscorefonts-installer fonts-liberation fonts-font-awesome conky tint2 neofetch dzen2 arc-theme -y
-	
+apt-get install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance gtk-theme-switch gtk-chtheme qt4-qtconfig xfonts-terminus ttf-mscorefonts-installer fonts-liberation fonts-font-awesome conky tint2 neofetch dzen2 arc-theme xdg-user-dirs rxvt-unicode numix-gtk-theme -y
+
 # Installing Audio/Visual tools:
 apt-get install moc vlc pulseaudio pavucontrol -y
-	
+
 # Installing GUI tools:
 apt-get install pcmanfm nitrogen mupdf rofi gimp gcolor2 agave -y
-	
+
 # Installing Additional tools:
-apt-get install wicd feh ranger tar nano sakura unrar rar xarchiver scrot -y
-	
+apt-get install wicd feh ranger tar nano sakura unrar rar xarchiver scrot alsa-utils -y
+
 # Installing Internet tools:
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update -y
 apt-get install google-chrome-stable filezilla -y
-	
+
+# Installing Fonts:
+git clone https://github.com/worron/ACYLS.git ~/.icons/ACYLS
+
+sleep 10s
+
 echo "Installation is now complete..."
